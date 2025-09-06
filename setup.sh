9b8de4349cd007f165523e951f1d3e3f45d581ab
Evan Phillips <evanap003300@gmail.com>
2025-09-05 21:59:59 -0400

    Revert "initial commit"
    This reverts commit 94ec93a5ebd7f3a6c35a1fda97f7454dbf80d404.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
-# For Python:
-python3 -m pip install -r backend/requirements.txt
+pip install -r backend/requirements.txt
 
-# For Node.js:
-npm install --prefix frontend
+cd frontend 
+npm install
